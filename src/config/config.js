9674516e9258922bac98589ad63c88d49e83331f
Fabricius97 <christoffer.fabricius@hotmail.com>
2023-06-28 13:54:04 +0200

    bug

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const authEndpoint = 'http://accounts.spotify.com/authorize';
-export const clientId = import.meta.env.VITE_CLIENT_ID;
-export const liveURL = import.meta.env.VITE_LIVE_URL;
+export const clientId = '9472f0d0b8134e66b6a8a197a4536eeb';
+export const liveURL = 'https://spotify-clone-sandy-sigma.vercel.app/';
 export const devURL = 'http://localhost:5173/';
 export const redirectURL = liveURL;
 export const scopes = [
